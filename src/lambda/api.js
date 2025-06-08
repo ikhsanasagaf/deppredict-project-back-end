1abd9c401ec4086e9ddf48183316e3a0c4cf5dc1
ikhsanasagaf <ikhsan.asagaf2012@gmail.com>
2025-06-08 20:28:11 +0700

    Revisi code untuk deployment

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -1,8 +1,8 @@
 const serverless = require('serverless-http');
-const { init } = require('../../src/server'); 
+const { init } = require('../server');
 
 module.exports.handler = async (event, context) => {
   const app = await init();
-  const handler = serverless(app);
+  const handler = serverless(app.listener);
   return await handler(event, context);
 };
